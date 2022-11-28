845d828d2b760e8a6537276aff23658d68210d21
draemonash2 <draemon_ash3@yahoo.co.jp>
2022-11-28 22:24:02 +0900

    mod

--- _sync_github-other.bat
+++ _sync_github-other.bat
@@ -1,6 +1,6 @@
 echo githubからダウンロードして比較します。
-"%MYDIRPATH_CODES%\vbs\tools\win\file_ope\UpdateCheck.vbs" "%~dp0" "https://github.com/draemonash2/other/archive/master.zip" "other-master"
+"%MYDIRPATH_CODES%\vbs\tools\win\file_ope\SyncGithubToCodes.vbs" "%~dp0" "https://github.com/draemonash2/other/archive/master.zip" "other-master"
 
 echo %MYDIRPATH_CODES%内の_localフォルダを比較します。
-"%MYDIRPATH_CODES%\vbs\tools\win\file_ope\DiffLocalDirs.vbs" "%~dp0"
+"%MYDIRPATH_CODES%\vbs\tools\win\file_ope\SyncCodesToLocal.vbs" "%~dp0"
 
